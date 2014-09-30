205b34b89610fd2fb10582ef1e5940313059180a
lclion <Himawari Sunshine@192.168.0.155>
2014-10-01 09:00:40 +1000

    New Project Hello World
    A test github repository

---
+++ HelloWorld/src/com/lclion/main/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lclion.main;
+
+public class HelloWorld
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("A very basic test for GitHub");
+		System.out.println("Hello World");
+	}
+
+}
