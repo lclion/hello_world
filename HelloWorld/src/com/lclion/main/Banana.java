e2cba24a9580a23b086f79540bc7425aa3230655
lclion <Himawari Sunshine@192.168.0.155>
2014-10-02 08:48:06 +1000

    Banana Class
    Added new function and var

--- HelloWorld/src/com/lclion/main/Banana.java
+++ HelloWorld/src/com/lclion/main/Banana.java
@@ -2,8 +2,16 @@ package com.lclion.main;
 
 public class Banana
 {
+	private boolean isPeeled = false;
+	
 	public Banana()
 	{
 		System.out.println("Bananas are delicious!");
+	}
+	
+	public void peelBanana()
+	{
+		isPeeled = true;
+		System.out.println("Banana: I'm cold and naked!");
 	}
 }
